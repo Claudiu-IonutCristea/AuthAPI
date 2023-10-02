37e6d82138dcac47d91b5897ed08b25a97ce56c4
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-10-02 23:07:24 +0300

    Add DataContext

---
+++ AuthAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthAPI.Data;
+
+public class DataContext : DbContext
+{
+	protected readonly IConfiguration _config;
+
+	public DataContext(IConfiguration config) : base()
+	{
+		_config = config;
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+	{
+		options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+	}
+}
