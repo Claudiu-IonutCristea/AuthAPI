283d926ef72d7cbd99ae6b86a0752c7a61ee7f7e
Claudiu-IonutCristea <claudiucristea12@gmail.com>
2023-10-02 23:07:37 +0300

    Add DataContextDevelopment

---
+++ AuthAPI/Data/DataContextDevelopment.cs
@@ -0,0 +1,14 @@
+﻿using AuthAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthAPI.Data;
+
+public class DataContextDevelopment : DataContext
+{
+	public DataContextDevelopment(IConfiguration config) : base(config) {}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder options)
+	{
+		options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+	}
+}
